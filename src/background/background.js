fff8ece11693fc25ef206075300a2162094c2a7d
Yuichi Tateno <hotchpotch@gmail.com>
2009-10-15 11:56:00 +0900

    コード分け

--- src/background/background.js
+++ src/background/background.js
@@ -1,11 +1,4 @@
 
-Deferred.define();
-
-var MY_NAME_URL = B_HTTP + 'my.name';
-// var MY_NAME_URL = '/tmpdata/my.name';
-var Database = Deferred.WebDatabase;
-// Database.debugMessage = true;
-var Model = Database.Model, SQL = Database.SQL;
 var Manager = $({});
 
 $.extend(Manager, {
@@ -13,91 +6,7 @@ $.extend(Manager, {
     }
 });
 
-var UserManager = $({});
 
-$.extend(UserManager, {
-    login: function() {
-        $.getJSON(MY_NAME_URL).next(UserManager.loginHandler).error(UserManager.loginErrorHandler); // .error(this.loginErrorHandler);
-    },
-    loginHandler: function(res) {
-        p('loginHandler');
-        UserManager.setUser(res);
-    },
-    loginErrorHandler: function UserManager_loginErrorHandler(res) {
-        p('login error...');
-        var j = JSON.parse(res.responseText);
-        UserManager.setUser(j);
-    },
-    logout: function UserManager_clearUser () {
-        UserManager.clearUser();
-    },
-    clearUser: function() {
-        if (UserManager.user) {
-            UserManager.user.clear();
-            delete UserManager.user;
-        }
-    },
-    setUser: function UserManager_setCurrentUser (res) {
-        if (!res.login) return;
-        var current = UserManager.user;
-        if (current) {
-            if (current.name == res.name) {
-                current.options.rks = res.rks;
-                current.options.plususer = res.plususer;
-                current.options.ignores_regex = res.ignores_regex;
-                delete current._ignores;
-                return current;
-            }
-            UserManager.clearUser();
-        }
-        var user = new User(res.name, res);
-        UserManager.user = user;
-        p('UserChange: ', user.name);
-        UserManager.trigger('UserChange', [user]);
-    }
-});
-
-var User = function(name, options) {
-    this._name = name;
-    this.options = options || {};
-};
-
-User.prototype = {
-    get name() { return this._name },
-    get plususer() { return this.options.plususer == 1 },
-    get rks() { return this.options.rks },
-    get private() { return this.options.private == 1 },
-    get public() { return !this.private },
-    get maxCommentLength() { return this.options.max_comment_length || 100 },
-    get ignores() {
-        if (this.options.ignores_regex) {
-            if (typeof this._ignores == 'undefined') {
-                try {
-                    this._ignores = new RegExp(this.options.ignores_regex);
-                } catch(e) {
-                    this._ignores = null;
-                }
-            }
-            return this._ignores;
-        }
-        return null;
-    },
-    hasBookmark: function user_hasBookmark(url) {
-        // var res = model('Bookmark').findByUrl(url);
-        // return res && res[0] ? true : false;
-    },
-    get database() {
-        return new Database('hatenabookmark-' + this.name, '1.0', 'hatenabookmark-' + this.name, 1024 * 1024 * 5);
-    },
-    get dataURL() { return sprintf(B_HTTP + '%s/search.data', this.name) },
-    // get dataURL() { return sprintf(B_HTTP + 'secondlife/search.data', this.name) },
-    // getProfileIcon: function user_getProfileIcon(isLarge) {
-    //     return UserUtils.getProfileIcon(this.name, isLarge);
-    // },
-
-    clear: function user_clear() {
-    }
-};
 
 $(document).ready(function() {
     UserManager.login();
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,4 +1,16 @@
 
+// consts
+var B_HOST = 'b.hatena.ne.jp';
+var B_HTTP = 'http://' + B_HOST + '/';
+var B_STATIC_HOST = 'b.st-hatena.com';
+var B_STATIC_HTTP = 'http://' + B_STATIC_HOST + '/';
+var B_API_STATIC_HOST = 'api.b.st-hatena.com';
+var B_API_STATIC_HTTP = 'http://' + B_API_STATIC_HOST + '/';
+
+var Database = Deferred.WebDatabase;
+// Database.debugMessage = true;
+var Model = Database.Model, SQL = Database.SQL;
+
 // utility
 var p = function() {
     console.log(JSON.stringify(Array.prototype.slice.call(arguments, 0, arguments.length)));
@@ -14,13 +26,6 @@ sprintf._SPRINTF_HASH = {
     '%d': parseInt,
     '%f': parseFloat,
 };
-
-var B_HOST = 'b.hatena.ne.jp';
-var B_HTTP = 'http://' + B_HOST + '/';
-var B_STATIC_HOST = 'b.st-hatena.com';
-var B_STATIC_HTTP = 'http://' + B_STATIC_HOST + '/';
-var B_API_STATIC_HOST = 'api.b.st-hatena.com';
-var B_API_STATIC_HTTP = 'http://' + B_API_STATIC_HOST + '/';
 
 var $K = function(i) { return function() { return i } };
 
@@ -50,7 +55,7 @@ if (typeof jQuery != 'undefined') {
     jQuery.extend = jQuery.fn.extend = function() {
         // copy reference to target object
         var target = arguments[0] || {}, i = 1, length = arguments.length, deep = false, options;
-    
+
         // Handle a deep copy situation
         if ( typeof target === "boolean" ) {
             deep = target;
@@ -58,17 +63,17 @@ if (typeof jQuery != 'undefined') {
             // skip the boolean and the target
             i = 2;
         }
-    
+
         // Handle case when target is a string or something (possible in deep copy)
         if ( typeof target !== "object" && !jQuery.isFunction(target) )
             target = {};
-    
+
         // extend jQuery itself if only one argument is passed
         if ( length == i ) {
             target = this;
             --i;
         }
-    
+
         for ( ; i < length; i++ )
             // Only deal with non-null/undefined values
             if ( (options = arguments[ i ]) != null )
@@ -82,11 +87,11 @@ if (typeof jQuery != 'undefined') {
                     } else {
                         copy = options[ name ];
                     }
-    
+
                     // Prevent never-ending loop
                     if ( target === copy )
                         continue;
-    
+
                     // Recurse if we're merging object values
                     if ( deep && !getterFlag && copy && typeof copy === "object" && !copy.nodeType ) {
                         src = target[ name ];
@@ -94,7 +99,7 @@ if (typeof jQuery != 'undefined') {
                             // Never move original objects, clone them
                             src || ( copy.length != null ? [ ] : { } )
                         , copy );
-    
+
                     // Don't bring in undefined values
                     } else {
                         if ( options.__lookupSetter__ && options.__lookupSetter__(name) !== undefined ) {
@@ -104,9 +109,9 @@ if (typeof jQuery != 'undefined') {
                             target[ name ] = copy;
                         }
                     }
-    
+
                 }
-    
+
         // Return the modified object
         return target;
     };
