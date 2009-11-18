ff393d3336315160c36e2bb66c5dca5ec6b1f95d
Yuichi Tateno <hotchpotch@gmail.com>
2009-11-18 21:20:21 +0900

    metadata impl

---
+++ src/content/widget_embedder.js
@@ -0,0 +1,303 @@
+// widget_embedder.js
+
+// XXX ToDo: Consider about namespaces.
+
+const B_HTTP = 'http://b.hatena.ne.jp/';
+const B_STATIC_HTTP = 'http://b.st-hatena.com/';
+
+var SiteinfoRequestor = {
+    init: function SR_init() {
+        var self = SiteinfoRequestor;
+        self.port = chrome.extension.connect();
+        self.port.onMessage.addListener(self.onMessage);
+        self.port.postMessage({
+            message: 'get_siteinfo_for_url',
+            data: { url: location.href },
+        });
+    },
+
+    destroy: function SR_destroy() {
+        var self = SiteinfoRequestor;
+        // XXX Can we remove the listener 'onMessage'?
+        self.port = null;
+    },
+
+    onMessage: function SR_onMessage(info) {
+        var self = SiteinfoRequestor;
+        if ('siteinfo' in info) {
+            self.onGotSiteinfo(info.siteinfo);
+            return;
+        }
+        if ('siteinfos' in info) {
+            self.onGotXPathSiteinfos(info.siteinfos);
+            return;
+        }
+    },
+
+    onGotSiteinfo: function SR_onGotSiteinfo(siteinfo) {
+        var self = SiteinfoRequestor;
+        if (siteinfo) {
+            if (!siteinfo.disable)
+                new WidgetEmbedder(siteinfo);
+            self.destroy();
+            return;
+        }
+        self.port.postMessage({ message: 'get_siteinfos_with_xpath' });
+    },
+
+    onGotXPathSiteinfos: function SR_onGotXPathSiteinfos(siteinfos) {
+        var self = SiteinfoRequestor;
+        for (var i = 0, n = siteinfos.length; i < n; i++) {
+            var siteinfo = siteinfos[i];
+            if (queryXPathOfType(siteinfo.domain, document,
+                                 XPathResult.BOOLEAN_TYPE)) {
+                if (!siteinfo.disable)
+                    new WidgetEmbedder(siteinfo);
+                break;
+            }
+        }
+        self.destroy();
+    },
+};
+
+function WidgetEmbedder(siteinfo) {
+    this.siteinfo = siteinfo;
+    this.embedLater(WidgetEmbedder.INITIAL_DELAY);
+    document.addEventListener('DOMNodeInserted', this, false);
+    document.addEventListener('AutoPagerize_DOMNodeInserted', this, false);
+    document.addEventListener('AutoPatchWork.DOMNodeInserted', this, false);
+}
+
+extend(WidgetEmbedder, {
+    INITIAL_DELAY:   30,
+    MUTATION_DELAY: 200,
+});
+
+extend(WidgetEmbedder.prototype, {
+    embedLater: function WE_embedLater(delay) {
+        if (this.timerId) return;
+        this.timerId = setTimeout(function (self) {
+            self.embed();
+            self.timerId = 0;
+        }, delay, this);
+    },
+
+    embed: function WE_embed() {
+        queryXPathAll(this.siteinfo.paragraph)
+            .forEach(this.embedInParagraph, this);
+    },
+
+    embedInParagraph: function WE_embedInParagraph(paragraph) {
+        if (paragraph._hb_isWidgetEmbedded) return;
+        paragraph._hb_isWidgetEmbedded = true;
+
+        var link = this.getLink(paragraph);
+        if (!link) return;
+        var point = this.getAnnotationPoint(paragraph, link);
+        if (!point) return;
+        var widgets = this.createWidgets(link);
+        point.insertNode(widgets);
+        point.detach();
+    },
+
+    getLink: function WE_getLink(paragraph) {
+        var xpath = this.siteinfo.link || '.';
+        if (xpath === '__location__') {
+            var url = location.href;
+            for (var node = paragraph; node; node = node.parentNode) {
+                if (node._hb_baseURL) {
+                    url = node._hb_baseURL;
+                    break;
+                }
+            }
+            var a = document.createElement('a');
+            a.href = url;
+            return a;
+        }
+        var link = queryXPath(xpath, paragraph);
+        return (link && link.href) ? link : null;
+    },
+
+    getAnnotationPoint: function WE_getAnnotationPoint(paragraph, link) {
+        var existing = this.getExistingWidgets(paragraph, link);
+        if (existing.counter) return null;
+        var point = document.createRange();
+        var anchor = existing.entry || existing.comments || existing.addButton;
+        if (anchor) {
+            point.selectNode(anchor);
+            point.collapse(anchor !== existing.entry);
+            return point;
+        }
+
+        var annotation = this.siteinfo.annotation
+                         ? queryXPath(this.siteinfo.annotation, paragraph)
+                         : link;
+        if (!annotation) return null;
+        var position = (this.siteinfo.annotationPosition || '').toLowerCase();
+        if (!position) {
+            switch (annotation.localName) {
+            case 'a': case 'br': case 'hr': case 'img': case 'canvas':
+            case 'object': case 'input': case 'button': case 'select':
+            case 'textarea':
+                position = 'after';
+                break;
+            default:
+                position = 'last';
+            }
+        }
+        if (position === 'before' || position === 'after')
+            point.selectNode(annotation);
+        else
+            point.selectNodeContents(annotation);
+        point.collapse(position === 'before' || position === 'start');
+        return point;
+    },
+
+    getExistingWidgets: function WE_getExistingWidgets(paragraph, link) {
+        const url = link.href;
+        const sharpEscapedURL = url.replace(/#/g, '%23');
+        const entryURL = getEntryURL(url);
+        const oldEntryURL = B_HTTP + 'entry/' + sharpEscapedURL;
+        const imageAPIPrefix = B_STATIC_HTTP + 'entry/image/';
+        const oldImageAPIPrefix = B_HTTP + 'entry/image/';
+        const addURL = B_HTTP + 'my/add.confirm?url=' + encodeURIComponent(url);
+        const oldAddURL = B_HTTP + 'append?' + sharpEscapedURL;
+        const entryImagePrefix = 'http://d.hatena.ne.jp/images/b_entry';
+        var widgets = {
+            entry:     null,
+            counter:   null,
+            comments:  null,
+            addButton: null,
+        };
+        queryXPathAll('descendant::a[@href]', paragraph).forEach(function (a) {
+            switch (a.href) {
+            case entryURL:
+            case oldEntryURL:
+                var content = a.firstChild;
+                if (!content) break;
+                if (content.nodeType === Node.TEXT_NODE) {
+                    if (content.nodeValue.indexOf(' user') !== -1) {
+                        var parentName = a.parentNode.localName;
+                        widgets.counter =
+                            (parentName === 'em' || parentName === 'strong')
+                            ? a.parentNode : a;
+                        break;
+                    }
+                    if (!content.nextSibling) break;
+                    content = content.nextSibling;
+                }
+                if (content.localName === 'img') {
+                    var src = content.src || '';
+                    if (src.indexOf(imageAPIPrefix) === 0 ||
+                        src.indexOf(oldImageAPIPrefix) === 0) {
+                        widgets.counter = a;
+                    } else if (src.indexOf(entryImagePrefix) === 0) {
+                        widgets.entry = a;
+                    }
+                }
+                break;
+
+            case addURL:
+            case oldAddURL:
+                widgets.addButton = a;
+                break;
+            }
+        }, this);
+        widgets.comments = paragraph.querySelector('.hatena-bcomment-view-icon');
+        return widgets;
+    },
+
+    createWidgets: function WE_createWidgets(link) {
+        var widgets = document.createDocumentFragment();
+        widgets.appendChild(document.createTextNode(' '));
+        var url = link.href;
+        var sharpEscapedURL = url.replace(/#/g, '%23');
+        widgets.appendChild(
+            E('a',
+              { href: getEntryURL(url), 'class': 'hBookmark-widget-counter' },
+              E('img',
+                { src: B_STATIC_HTTP + 'entry/image/' + sharpEscapedURL, alt: '' }))
+        );
+        return widgets;
+    },
+
+    handleEvent: function WE_handleEvent(event) {
+        switch (event.type) {
+        case 'AutoPagerize_DOMNodeInserted':
+        case 'AutoPatchWork.DOMNodeInserted':
+            document.removeEventListener('DOMNodeInserted', this, false);
+            event.target._hb_baseURL = event.newValue;
+            /* FALL THROUGH */
+        case 'DOMNodeInserted':
+            this.embedLater(WidgetEmbedder.MUTATION_DELAY);
+            break;
+        }
+    },
+});
+
+function extend(dest, src) {
+    for (var i in src)
+        dest[i] = src[i];
+    return dest;
+}
+
+function queryXPath(xpath, context) {
+    return queryXPathOfType(xpath, context,
+                            XPathResult.FIRST_ORDERED_NODE_TYPE);
+}
+
+function queryXPathAll(xpath, context) {
+    return queryXPathOfType(xpath, context,
+                            XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE);
+}
+
+function queryXPathOfType(xpath, context, type) {
+    context = context || document;
+    var doc = context.ownerDocument || context;
+    var result = doc.evaluate(xpath, context, null, type, null);
+
+    switch (result.resultType) {
+    case XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE:
+    case XPathResult.ORDERED_NODE_SNAPSHOT_TYPE:
+        var nodes = [];
+        for (var i = 0, n = result.snapshotLength; i < n; i++)
+            nodes.push(result.snapshotItem(i));
+        return nodes;
+    case XPathResult.ANY_UNORDERED_NODE_TYPE:
+    case XPathResult.FIRST_ORDERED_NODE_TYPE:
+        return result.singleNodeValue;
+
+    case XPathResult.NUMBER_TYPE:  return result.numberValue;
+    case XPathResult.STRING_TYPE:  return result.stringValue;
+    case XPathResult.BOOLEAN_TYPE: return result.booleanValue;
+    case XPathResult.UNORDERED_NODE_ITERATOR_TYPE:
+    case XPathResult.ORDERED_NODE_ITERATOR_TYPE:
+        return result;
+    }
+    throw new Error("Unknown XPath result type.");
+}
+
+function E(name, attrs) {
+    var element = document.createElement(name);
+    for (var a in attrs)
+        element.setAttribute(a, attrs[a]);
+    for (var i = 2, n = arguments.length; i < n; i++) {
+        var child = arguments[i];
+        if (!child.nodeType)
+            child = document.createTextNode(child);
+        element.appendChild(child);
+    }
+    return element;
+}
+
+function getEntryURL(url) {
+    var suffix = url.replace(/#/g, '%23');
+    if (suffix.indexOf('http://') === 0)
+        suffix = suffix.substring(7);
+    else if (suffix.indexOf('https://') === 0)
+        suffix = 's/' + suffix.substring(8);
+    return B_HTTP + 'entry/' + suffix;
+}
+
+
+SiteinfoRequestor.init();
---
+++ src/lib/10-SiteinfoManager.js
@@ -0,0 +1,257 @@
+var SiteinfoManager = $({});
+
+$.extend(SiteinfoManager, {
+    init: function SM_init() {
+        console.log('SiteinfoManager loaded');
+        var self = SiteinfoManager;
+        self.timerId = setInterval(self.updateSiteinfos, 10 * 60 * 1000);
+    },
+
+    sendSiteinfoForURL: function SM_sendSiteinfoForURL(url, port) {
+        console.log('received siteinfo request for ' + url);
+        var self = SiteinfoManager;
+        var result = null;
+        var list = self.siteinfosList;
+        SEARCH:
+        for (var i = 0; i < list.length; i++) {
+            var siteinfos = list[i].data;
+            for (var j = 0, n = siteinfos.length; j < n; j++) {
+                var siteinfo = siteinfos[j];
+                var pattern = siteinfo.domainPattern;
+                if (!pattern) {
+                    try {
+                        pattern = siteinfo.domainPattern = new RegExp(siteinfo.domain);
+                    } catch (ex) {
+                        siteinfos.splice(j, 1);
+                        j--;
+                        n--;
+                        continue;
+                    }
+                }
+                if (pattern.test(url)) {
+                    result = siteinfo;
+                    break SEARCH;
+                }
+            }
+        }
+        console.log(result);
+        port.postMessage({ siteinfo: result });
+    },
+
+    sendSiteinfosWithXPath: function SM_sendSiteinfosWithXPath(port) {
+        var self = SiteinfoManager;
+        var result = [];
+        var list = self.siteinfosList;
+        for (var i = 0; i < list.length; i++) {
+            var details = list[i];
+            if (details.xpathData)
+                result = result.concat(details.xpathData);
+        }
+        port.postMessage({ siteinfos: result });
+    },
+
+    siteinfosList: [],
+
+    addSiteinfos: function SM_addSiteinfos(details) {
+        var self = SiteinfoManager;
+        if (!details.data)
+            details.data = [];
+        self.siteinfosList.push(details);
+        self.updateSiteinfos();
+    },
+
+    UPDATE_INTERVAL: 24 * 60 * 60 * 1000,
+
+    updateSiteinfos: function SM_updateSiteinfos() {
+        var self = SiteinfoManager;
+        self.siteinfosList.forEach(function (details) {
+            if (!details.urls) return;
+            if (!details.lastUpdated ||
+                details.lastUpdated + self.UPDATE_INTERVAL < Date.now())
+                self.fetchSiteinfos(details.urls.concat(), details);
+        });
+    },
+
+    REQUEST_TIMEOUT: 3 * 60 * 1000,
+
+    fetchSiteinfos: function SM_fetchSiteinfos(urls, details) {
+        if (details.isLoading) return;
+        details.isLoading = true;
+        var self = SiteinfoManager;
+        var url = urls.shift();
+        $.getJSON(url).next(function (data) {
+            console.log('load siteinfo from ' + url);
+            details.isLoading = false;
+            if (details.converter)
+                data = details.converter.convert(data, details);
+            details.data = data;
+            details.lastUpdated = Date.now();
+        }).error(function () {
+            details.isLoading = false;
+            if (urls.length)
+                self.fetchSiteinfos(urls, details);
+        });
+    },
+});
+
+
+SiteinfoManager.LDRizeConverter = {
+    convert: function SM_LDR_convert(data, details) {
+        var urlPatternPattern = /^\^?http(?:s\??)?:/;
+        var evaluator = new XPathEvaluator();
+        details.xpathData = [];
+        return data.map(function (detaildData) {
+            return detaildData.data;
+        }).filter(function (data) {
+            if (!data.domain) return false;
+            if (urlPatternPattern.test(data.domain)) return true;
+            try {
+                evaluator.createExpression(data.domain, null);
+                details.xpathData.push(data);
+                return false;
+            } catch (ex) {}
+            try {
+                new RegExp(data.domain);
+                return true;
+            } catch (ex) {}
+            return false;
+        });
+    },
+};
+
+SiteinfoManager.SiteconfigConverter = {
+    convert: function SM_SC_convert(data) {
+        var self = SiteinfoManager.SiteconfigConverter;
+        var result = [];
+        for (var domain in data) {
+            var entries = data[domain];
+            var domainPattern = '^https?://';
+            domainPattern += (domain.charCodeAt(0) === 42 /* '*' */)
+                ? '(?:[\w-]+\.)*' + domain.substring(2).replace(/\W/g, '\\$&')
+                : domain.replace(/\W/g, '\\$&');
+            for (var i = 0; i < entries.length; i++) {
+                var entry = entries[i];
+                var path = entry.path || '';
+                var urlPattern = domainPattern +
+                                 ((path.charCodeAt(0) === 94 /* '^' */)
+                                  ? path.substring(1)
+                                  : '.*' + path);
+                var entryNodes = entry.entryNodes;
+                if (!entryNodes) continue;
+                var paraSelector;
+                // 同一パス下での複数エントリには非対応
+                for (var paraSelector in entryNodes) break;
+                var paraInfos = entryNodes[paraSelector];
+                var siteinfo = {
+                    domain:    urlPattern,
+                    paragraph: selector2xpath(paraSelector),
+                    link:      selector2xpath(paraInfos.uri || 'parent'),
+                }
+                if (paraInfos.container)
+                    siteinfo.annotation = selector2xpath(paraInfos.container);
+                result.push(siteinfo);
+            }
+        }
+        return result;
+
+        function selector2xpath(selector) {
+            switch (selector) {
+            case 'parent':
+                return 'self::*';
+
+            case 'window.location':
+            case 'document.location':
+                return '__location__';
+
+            //case 'document.title':
+            //    return '""';
+            }
+
+            selector = selector.replace(/\s+/g, ' ').replace(/ ?([,>]) ?/g, '$1');
+            return selector.replace(/(,)?([^,]+)/g, singleSelector2xpath);
+        }
+
+        function singleSelector2xpath(match, comma, selector) {
+            return (comma ? ' | descendant::' : 'descendant::') +
+                selector.replace(/([ >])?([^ >]+)/g, selectorPart2xpath);
+        }
+
+        function selectorPart2xpath(match, combinator, part) {
+            var step = combinator ? ((combinator === ' ') ? '/descendant::' : '/child::') : ''
+            var elementEnd = part.search(/[#.:]/);
+            if (elementEnd === -1) return step + part;
+            if (elementEnd) {
+                step += part.substring(0, elementEnd);
+                part = part.substring(elementEnd);
+            } else {
+                step += '*';
+            }
+            return step + '[' + part.replace(/([#.:])([\w-]+(?:\((\d+)\))?)/g, classes2xpath) + ']';
+        }
+
+        function classes2xpath(match, kind, name, index, offset) {
+            var expr = offset ? ' and ' : '';
+            if (kind === '#')
+                return expr + '@id = "' + name + '"';
+            if (kind === '.')
+                return expr + 'contains(concat(" ", normalize-space(@class), " "), " ' + name + ' ")';
+
+            switch (name) {
+            case 'first-child':
+            case 'first': // Someone specifies incorrect pseudo classes
+            case 'nth-child(1)':
+                return expr + 'not(preceding-sibling::*)';
+
+            case 'last-child':
+                return expr + 'not(following-sibling::*)';
+            }
+            // Assume it is an nth-child pseudo class.
+            return expr + 'count(preceding-sibling::*) = ' + (index - 1 || -1);
+        }
+    },
+};
+
+
+SiteinfoManager.addSiteinfos({
+    data: [
+        { // Google Web Search
+            domain:     '^http://www\\.google(?:\\.\\w+){1,2}/search\\?',
+            paragraph:  'descendant::div[@id = "res"]/div/ol/li[contains(concat(" ", @class, " "), " g ")]',
+            link:       'descendant::a[contains(concat(" ", @class, " "), " l ")]',
+            annotation: 'descendant::span[contains(concat(" ", @class, " "), " gl ")]',
+            annotationPosition: 'after',
+        },
+        {
+            domain:  '^http://b\\.hatena\\.ne\\.jp/',
+            disable: true,
+        },
+    ],
+});
+
+SiteinfoManager.addSiteinfos({
+    urls: [
+        'http://wedata.net/databases/HatenaBookmarkUsersCount/items.json',
+        'http://b.st-hatena.com/file/HatenaBookmarkUsersCount.items.json',
+    ],
+    converter: SiteinfoManager.LDRizeConverter,
+});
+
+
+
+SiteinfoManager.addSiteinfos({
+    urls: [
+        'http://wedata.net/databases/LDRize/items.json',
+        'http://b.st-hatena.com/file/LDRize.items.json',
+    ],
+    converter: SiteinfoManager.LDRizeConverter,
+});
+
+SiteinfoManager.addSiteinfos({
+    urls: [
+        'http://s.hatena.ne.jp/siteconfig.json',
+    ],
+    converter: SiteinfoManager.SiteconfigConverter,
+});
+
+
+SiteinfoManager.init();
