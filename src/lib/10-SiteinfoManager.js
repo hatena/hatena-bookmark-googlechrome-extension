24c2f3784ddfe6e58ef4afcd0ab06302d2425438
nanto_vi <nanto@moon.email.ne.jp>
2009-11-25 12:54:46 +0900

    個別のSITEINFOの情報をSiteinfoManagerから分離。

--- src/lib/10-SiteinfoManager.js
+++ src/lib/10-SiteinfoManager.js
@@ -51,13 +51,14 @@ $.extend(SiteinfoManager, {
     },
 
     siteinfosList: [],
+    storage: localStorage,
 
     CACHE_KEY_PREFIX: 'SiteinfoCache.',
 
     addSiteinfos: function SM_addSiteinfos(details) {
         var self = SiteinfoManager;
         if (details.key) {
-            var cache = localStorage[self.CACHE_KEY_PREFIX + details.key];
+            var cache = self.storage[self.CACHE_KEY_PREFIX + details.key];
             if (cache) {
                 cache = JSON.parse(cache);
                 details.data = cache.data;
@@ -101,7 +102,7 @@ $.extend(SiteinfoManager, {
             details.data = data;
             details.lastUpdated = Date.now();
             if (details.key) {
-                localStorage[self.CACHE_KEY_PREFIX + details.key] =
+                self.storage[self.CACHE_KEY_PREFIX + details.key] =
                     JSON.stringify(details);
             }
         }).error(function () {
@@ -228,51 +229,6 @@ SiteinfoManager.SiteconfigConverter = {
         }
     },
 };
-
-
-SiteinfoManager.addSiteinfos({
-    data: [
-        { // Google Web Search
-            domain:     '^http://www\\.google(?:\\.\\w+){1,2}/search\\?',
-            paragraph:  'descendant::div[@id = "res"]/div/ol/li[contains(concat(" ", @class, " "), " g ")]',
-            link:       'descendant::a[contains(concat(" ", @class, " "), " l ")]',
-            annotation: 'descendant::span[contains(concat(" ", @class, " "), " gl ")]',
-            annotationPosition: 'after',
-        },
-        {
-            domain:  '^http://b\\.hatena\\.ne\\.jp/',
-            disable: true,
-        },
-    ],
-});
-
-SiteinfoManager.addSiteinfos({
-    urls: [
-        'http://wedata.net/databases/HatenaBookmarkUsersCount/items.json',
-        'http://b.st-hatena.com/file/HatenaBookmarkUsersCount.items.json',
-    ],
-    converter: SiteinfoManager.LDRizeConverter,
-    key: 'HatenaBookmarkUsersCount',
-});
-
-
-
-SiteinfoManager.addSiteinfos({
-    urls: [
-        'http://wedata.net/databases/LDRize/items.json',
-        'http://b.st-hatena.com/file/LDRize.items.json',
-    ],
-    converter: SiteinfoManager.LDRizeConverter,
-    key: 'LDRizeSiteinfo',
-});
-
-SiteinfoManager.addSiteinfos({
-    urls: [
-        'http://s.hatena.ne.jp/siteconfig.json',
-    ],
-    converter: SiteinfoManager.SiteconfigConverter,
-    key: 'HatenaStarSiteConfig',
-});
 
 
 SiteinfoManager.init();
