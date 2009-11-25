4dce7eefcf0e9de877c75da2b9babb6371faf410
Yuichi Tateno <hotchpotch@gmail.com>
2009-11-25 16:18:58 +0900

    add wasure

---
+++ src/content/bookmarkedit_bridge.js
@@ -0,0 +1,64 @@
+
+
+var port;
+function init() {
+    port = chrome.extension.connect();
+    port.onMessage.addListener(onMessage);
+    port.postMessage({
+        message: 'bookmarkedit_bridge_set',
+        data: { url: location.href },
+    });
+}
+
+function onMessage(info) {
+    var data = {
+        url: location.href,
+    };
+    var canURL = getCannonical();
+    if (canURL)
+        data.canonical = canURL;
+    var images = getImages();
+    if (images && images.length)
+        data.images = images;
+
+    port.postMessage({
+        message: 'bookmarkedit_bridge_recieve',
+        data: data,
+    });
+}
+
+function getCannonical() {
+    var link = document.evaluate(
+        '/h:html/h:head/h:link[translate(@rel, "CANONICAL", "canonical") = "canonical"]',
+        document,
+        function () { return document.documentElement.namespaceURI || "" },
+        XPathResult.FIRST_ORDERED_NODE_TYPE,
+        null
+    ).singleNodeValue;
+    if (!link || !link.href) return null;
+    var url = link.href;
+    if (location.href == url) return null;
+    return url;
+}
+
+function getImages() {
+    var images = Array.prototype.filter.call(document.getElementsByTagName("img"),
+        function (img) { return (img instanceof HTMLImageElement) });
+    var maxCount = 20;
+    if (images.length > maxCount) {
+        images = images.map(function (image, index) {
+            var size = Math.min(image.naturalWidth,
+                                image.naturalHeight);
+            return { image: image, size: size, index: index };
+        }).sort(function (a, b) { return b.size - a.size })
+          .slice(0, maxCount)
+          .sort(function(a, b) { return a.index - b.index })
+          .map(function (item) { return item.image });
+    }
+    return images.filter(function(image) { return image && image.src }).map(function(image) { return image.src });
+}
+
+
+if (window.top == window.self)
+    init();
+
